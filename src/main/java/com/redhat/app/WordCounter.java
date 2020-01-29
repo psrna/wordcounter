818007bb0e5437445e3e7e6a58bc32583cba9ddd
Veronika Andrlova <xandrlov@fi.muni.cz>
2020-01-29 13:37:42 +0100

    missing tests

--- src/main/java/com/redhat/app/WordCounter.java
+++ src/main/java/com/redhat/app/WordCounter.java
@@ -1,13 +1,38 @@
 package com.redhat.app;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Counts number of words across files specified as arguments.
  *
  */
-public class WordCounter 
+public class WordCounter
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Map<String, Integer> words = new HashMap<String, Integer>();
+
+        for (String arg : args) {
+            try (BufferedReader br = new BufferedReader(new FileReader(arg))) {
+                String line;
+
+                while (null != (line = br.readLine())) {
+                    String[] string = line.toLowerCase().split("[^\\p{L}]+");
+
+                    for (String word : string) {
+                        words.put(word, words.getOrDefault(word, 0)+1);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : words.entrySet()) {
+            System.out.println(entry.getKey() +": " + entry.getValue());
+        }
     }
 }
